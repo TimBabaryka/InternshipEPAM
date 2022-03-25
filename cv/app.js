d37bdfc4f51072899ea875381efc5d752c73a6fc
Tim Babaryka <timasky95@gmail.com>
2022-03-25 17:45:37 +0300

    refactor: fix

---
+++ cv/app.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  let a = document.getElementById("myMenu");
+  if (a.style.display === "block") {
+    a.style.display = "none";
+  } else {
+    a.style.display = "block";
+  }
+}
