7a15d0ebd74dca13281626924091275ce6c9c869
Tim Babaryka <timasky95@gmail.com>
2022-04-06 16:43:20 +0300

    refactor: del anim and cv

--- cv/app.js
+++
@@ -1,8 +0,0 @@
-function toggleMenu() {
-  let a = document.getElementById("myMenu");
-  if (a.style.display === "block") {
-    a.style.display = "none";
-  } else {
-    a.style.display = "block";
-  }
-}
